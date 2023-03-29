ee6513091f0a066bfd4d15fbb1f924e0be27c533
Jamie Wright <jwright3478@gmail.com>
2023-03-29 16:22:33 +0100

    error page

--- assets/js/generic-render.js
+++ assets/js/generic-render.js
@@ -1,9 +1,24 @@
 function render(article, year) {
   fetch(`/articles/${year}/${article}.html`)
-    .then((response) => response.text())
+    .then((response) => {
+      console.log(response);
+      if (response.ok) {
+        return response.text();
+      } else {
+        throw new Error();
+      }
+    })
     .then((template) => {
       const rendered = Mustache.render(template);
       document.getElementById("target").innerHTML = rendered;
+    })
+    .catch((error) => {
+      fetch(`/assets/html/error.html`)
+        .then((response) => response.text())
+        .then((template) => {
+          const rendered = Mustache.render(template);
+          document.getElementById("target").innerHTML = rendered;
+        });
     });
 
   fetch(`/assets/html/footer.html`)
